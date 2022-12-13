cab11342e0e770a0d4939d673b95904e8c66a693
Ori Menachem Levi <orimenachemlevi@gmail.com>
2022-12-13 22:39:20 +0200

    try catch closures to auth controller functions

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -8,93 +8,103 @@ import { ConnectionKeysBE } from '../models/auth/mdb/connectionKey.mdb';
 import { UserRegisterModel } from '../models/auth/general/UserRegister.model';
 
 
-function LoginByEmailAndPassword(req: Request | any, res: Response) {
+async function LoginByEmailAndPassword(req: Request | any, res: Response) {
+    try {
 
-    const { email, password } = req.body;
+        const { email, password } = req.body;
 
-    const userCriteria: LoginRequestModel = { email, password };
+        const userCriteria: LoginRequestModel = { email, password };
 
-    AuthService.AuthUserByCriteria(userCriteria)
-        .then((authUser) => {
-            // check if user password is valid
-            if (authUser != null) {
-                const isUserPasswordValid = AuthService.ComparePasswordAction(password, authUser.hash);
-                if (isUserPasswordValid) {
-                    // get user from db
-                    UserService.getUserByEmail(email)
-                        .then((user) => {
-                            if (user) {
-                                const userId = user?._id.toString();
-                                const tokens = AuthService.CreateJWTTokensAction(userId);
-                                const userResponse: UserResponseModel = {
-                                    name: null,
-                                    email: email,
-                                    token: tokens.reqToken
-                                };
+        const authUser = await AuthService.AuthUserByCriteria(userCriteria)
+        // check if user password is valid
+        if (authUser != null) {
 
-                                res.cookie('jwt', tokens.refToken, {
-                                    httpOnly: true,
-                                    sameSite: 'none',
-                                    secure: true,
-                                    maxAge: 24 * 60 * 60 * 1000
-                                });
+            const isUserPasswordValid = AuthService.ComparePasswordAction(password, authUser.hash);
 
-                                return res.send(userResponse);
-                            }
-                        })
-                        .catch((err) => {
-                            throw new Error(err);
-                        })
+            if (isUserPasswordValid) {
+                // get user from db
+                const user = await UserService.getUserByEmail(email);
+
+                if (user) {
+                    const userId = user._id.toString();
+                    const tokens = AuthService.CreateJWTTokensAction(userId);
+                    const userResponse: UserResponseModel = {
+                        name: user.name,
+                        email: email,
+                        token: tokens.reqToken
+                    };
+
+                    res.cookie('jwt', tokens.refToken, {
+                        httpOnly: true,
+                        sameSite: 'none',
+                        secure: true,
+                        maxAge: 24 * 60 * 60 * 1000
+                    });
+
+                    return res.send(userResponse);
                 }
+
             } else {
-                return res.sendStatus(401);
+                throw Error('סיסמא שגויה');
             }
-        })
-        .catch(err => {
-            res.status(404).send({ message: err.message });
-        })
+        } else {
+            throw Error('משתמש לא רשום')
+        }
+    } catch (error) {
+        if (error instanceof Error)
+            return res.status(500).send(error.message)
+
+        throw Error('unkown');
+    }
 };
 
 async function CreateUserByEmailAndPasswordAction(req: Request, res: Response, next: NextFunction) {
+    try {
 
-    const { email, password, name } = req.body;
-    // hash password
-    const hash = await AuthService.HashPasswordAction(password);
+        const { email, password, name } = req.body;
+        // hash password
+        const hash = await AuthService.HashPasswordAction(password);
 
-    if (hash != null) {
+        if (hash != null) {
 
-        const model: ConnectionKeysBE = { email, hash: hash };
-        // create user auth doc and user doc
-        const userAuth = await AuthService.CreateUserConnectionKeysByEmailAndPassword(model);
+            const model: ConnectionKeysBE = { email, hash: hash };
+            // create user auth doc and user doc
+            const userAuth = await AuthService.CreateUserConnectionKeysByEmailAndPassword(model);
 
-        const userRegisterModel: UserRegisterModel = {
-            email,
-            name,
-            userAuthId: userAuth._id as string
-        };
+            const userRegisterModel: UserRegisterModel = {
+                email,
+                name,
+                userAuthId: userAuth._id as string
+            };
 
-        const user = await AuthService.CreateUserByEmailAndPassword(userRegisterModel)
-        // create jwt request and refresh tokens
-        const tokens = AuthService.CreateJWTTokensAction(user._id as string); // {reqToken, refToken}
+            const user = await AuthService.CreateUserByEmailAndPassword(userRegisterModel)
+            // create jwt request and refresh tokens
+            const tokens = AuthService.CreateJWTTokensAction(user._id as string); // {reqToken, refToken}
 
-        const userResponse: UserResponseModel = {
-            name,
-            email,
-            token: tokens.reqToken
-        };
+            const userResponse: UserResponseModel = {
+                name,
+                email,
+                token: tokens.reqToken
+            };
 
-        res.cookie('jwt', tokens.refToken, {
-            httpOnly: true,
-            sameSite: 'none',
-            secure: true,
-            maxAge: 24 * 60 * 60 * 1000
-        });
+            res.cookie('jwt', tokens.refToken, {
+                httpOnly: true,
+                sameSite: 'none',
+                secure: true,
+                maxAge: 24 * 60 * 60 * 1000
+            });
 
-        return res.send(userResponse);
+            return res.send(userResponse);
+        }
+
+        throw Error(`שגיאת שרת`);
+
+    } catch (error) {
+        if (error instanceof Error)
+            return res.status(500).send(error.message)
+
+        throw Error('unkown');
     }
-
-    return res.status(500).send(`שגיאת שרת`);
-
 };
 
 export default {
